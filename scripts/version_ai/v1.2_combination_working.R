8659652305a8fcdd19091bc630dfa1591da2832a
Ingram Thomas <Thomas.Ingram@wwl.nhs.uk>
2022-12-24 15:08:56 +0000

    clean combi script

--- scripts/version_ai/v1.2_combination_working.R
+++ scripts/version_ai/v1.2_combination_working.R
@@ -1,19 +1,38 @@
-testing <- wl_2  %>%
-  fill_gaps() %>%
-  fill(speciality, .direction = "up") %>%
-  group_by(speciality) %>%
-  fill(patients, .direction = "up")
+#####
+# Time period models trained on
+train_init = "2022-06-01"
+train_halt = "2022-11-01"
 
 
-train <- wl_2 %>%
-  filter(month(date) > 6) %>%
-  select(date, patients)
+--------------------------------------------------------------------------------
+#####
+# Filter to init date, filling date gaps and imputing missing wl size
+wl_prepared <- wl %>%
+  filter(., date > train_init) %>%
+  as_tsibble(.) %>%
+  fill_gaps(., date) %>%
+  fill(., speciality, .direction = "up") %>%
+  group_by(., speciality) %>%
+  fill(., patients, .direction = "up") %>%
+  ungroup(.)
 
+# Filter to halt date
+train_set <- wl_prepared %>%
+  filter(., date < train_halt) %>%
+  select(., date, patients)
+
+# horizon (days)
+h <- nrow(wl_prepared) - nrow(train_set)
+
+
+--------------------------------------------------------------------------------
+##### 
+# Build forecasting models to test
 STLF <- decomposition_model(
   STL((patients) ~ season(window = Inf)),
   ETS(season_adjust ~ season("N"))
 )
-cafe_models <- train %>%
+model_frame <- train_set %>%
   fill_gaps(patients = mean(patients)) %>%
   fabletools::model(
     ets = ETS(patients, trace = T),
@@ -23,23 +42,26 @@ cafe_models <- train %>%
   ) %>%
   mutate(combination = (ets + stlf + arima + nnar)/4)
 
+
+--------------------------------------------------------------------------------
+#####
 # Generate future sample paths
-cafe_futures <- cafe_models %>%
-  generate(h = 300, times = 25)
+sim_paths <- model_frame %>%
+  generate(h = h, times = 25)
 
 # Compute forecast distributions from future sample paths and create fable object
-cafe_fc2 <- cafe_futures %>%
+sim_results <- sim_paths %>%
   as_tibble() %>%
   group_by(date, .model) %>%
   summarise(dist = distributional::dist_sample(list(.sim))) %>%
   ungroup() %>%
   as_fable(index=date, key=.model, distribution=dist, response="patients")
 
-
-cafe_fc2 %>%
+# Plot results over-laid on wl and filter for combination model
+sim_results %>%
   #filter(.model == "combination") %>%
-  autoplot(wl_2, level = 80, size = 1, alpha = 0.5)
+  autoplot(wl, level = 80, size = 1, alpha = 0.5)
 
 # Check accuracy of 95% prediction intervals
-cafe_fc2 %>% accuracy(wl_2, measures = interval_accuracy_measures, level=95)
-         
+sim_results %>% accuracy(wl, measures = interval_accuracy_measures, level=95)
+        
