fff1e5019ad9f51e16596726c620d4f2aaa242b9
Ingram Thomas <Thomas.Ingram@wwl.nhs.uk>
2023-03-11 17:20:28 +0000

    try pull QVD and remake. Capacity looks wrong

--- scripts/current/sourced_scripts/sourced-v2.2-parameter_est.R
+++ scripts/current/sourced_scripts/sourced-v2.2-parameter_est.R
@@ -1,8 +1,8 @@
 source(here("scripts/archive/db_connect_functions.R"))
 source(here("scripts/archive/helper_functions.R"))
-
+{
 # specialities <- speciality
-speciality = c("Trauma & Orthopaedics")
+speciality = c("Urology")
 wl_type = c("Inpatient_wl", "Planned")
 
 ###### PARAMS #####
@@ -12,7 +12,7 @@ labels = month(date_to, label = T)
 
 parameters <-
   data.frame(
-    specialty = NA,
+    speciality = NA,
     time_period = NA,
     metric = NA,
     list = NA,
@@ -21,7 +21,7 @@ parameters <-
 
 parameters_lw <-
   data.frame(
-    specialty = NA,
+    speciality = NA,
     time_period = NA,
     metric = NA,
     list = NA,
@@ -43,94 +43,107 @@ data_recent <- data_mutate %>%
          covid_recovery_priority, wm52, wm65) %>%
   mutate(rtt_days_wait = if_else(rtt_days_wait < 0, 0, rtt_days_wait)) %>%
   rename(dt = decision_to_admit_date_dt,
-         speciality = spec_desc) %>%
+         speciality = spec_desc) #%>%
   group_by(RTT_Start_Date, dt, admission_date_dt, 
            removed_date, rott, speciality, internal_number, pathway_number,
            covid_recovery_priority, wm52, wm65) %>%
     summarise(
-      rtt_days_wait = max(rtt_days_wait))
+      rtt_days_wait = max(rtt_days_wait)) %>%
+  group_by(covid_recovery_priority) %>%
+  summarise(count = n())
 
 # Number of DTAs during the period. For demand we just need number of DTAs per day of the period.
-for (i in wl_type){
-  demand <- data_recent %>%
-    filter(covid_recovery_priority == i) 
+for (j in speciality){
+  
+  days <-
+    data.frame(dt = seq(
+      from = (train_halt - 30),
+      to = train_halt,
+      by = 1
+    ))
+  wl_type = c("Inpatient_wl", "Planned")
+  demand_daily <- data_recent %>%
+    filter(speciality == j,
+           covid_recovery_priority != "Unknown") %>%
+    group_by(covid_recovery_priority, dt) %>%
+    summarise(dtas = n()) %>%
+    ungroup(.) %>%
+    group_by(covid_recovery_priority) %>%
+    pad() %>%
+    mutate(dtas = if_else(is.na(dtas), 0, as.numeric(dtas))) %>%
+    filter(dt >= (train_halt - 30))
+
     
-    ### daily average
-    days <-
-      data.frame(dt = seq(
-        from = (train_halt - 30),
-        to = train_halt,
-        by = 1
-      ))
-    dta_daily <- data_recent  %>%
-      group_by(covid_recovery_priority, speciality, dt) %>%
-      summarise(dtas = n()) %>%
-      ungroup(.) %>%
-      right_join(days) %>%
-      group_by(covid_recovery_priority, speciality) %>%
-      pad() %>%
-      ungroup(.) %>%
-      mutate(dtas = if_else(is.na(dtas), 0, as.numeric(dtas)))
-    
-    for (j in speciality) {
-      if (!j %in% dta_daily$speciality) {
-        dta_daily <- rbind(
-          dta_daily,
-          data.frame(covid_recovery_priority = "Planned",
-                     speciality = j,
-                     dt = train_halt,
-                     dtas = 0)
-        )
-      }
+  for (wl in wl_type) {
+    if (!wl %in% demand_daily$covid_recovery_priority) {
+      demand_daily <- rbind(
+        demand_daily,
+        data.frame(covid_recovery_priority = wl,
+                   dt = train_halt,
+                   dtas = 0)
+      )
     }
-    
-    dta_mean <- dta_daily %>%
-      group_by(covid_recovery_priority, speciality) %>%
+  }
+    demand_mean <- demand_daily %>%
+      group_by(covid_recovery_priority) %>%
       summarise(month_mean = sum(dtas)/30)
     
-    ggplot(data = dta_daily, aes(x = dt, y = dtas, colour = covid_recovery_priority)) +
+    ggplot(data = demand_daily, aes(x = dt, y = dtas, colour = covid_recovery_priority)) +
       geom_smooth(se = F)
     
-
-
-inv_unk <- data_recent %>%
-  filter(covid_recovery_priority == "Unknown") %>%
-  group_by(pathway_number) %>%
-  summarise(n = n())
-  group_by(is.na(decision_to_admit_date_dt), is.na(admission_date_dt), is.na(removed_date)) #%>%
-  # summarise(n = n())
-  # filter(covid_recovery_priority == c("Planned", "Inpatient_wl")) %>%
-  group_by(
-    internal_number,
-    pathway_number,
-    decision_to_admit_date_dt,
-    admission_date_dt,
-    removed_date,
-    Admis_Method_Desc,
-    covid_recovery_priority
-  ) %>%
-  summarise(
-    rtt_days_wait = max(rtt_days_wait)
-  ) %>%
-  ungroup()
-  
-
-dtas <- dtas %>%
-  mutate(decision_to_admit_date_dt = date(dta_date)) %>%
-  mutate(clock_start_date = date(clock_start_date)) %>%
-  mutate(dta_date = date(dta_date)) %>%
-  mutate(clock_stop_date = date(clock_stop_date)) %>%
-  mutate(clock_start_date = if_else(Admis_Method_Desc %in% c("ELECTIVE WAITING LIST", "ELECTIVE BOOKED") & is.na(clock_start_date), dta_date, clock_start_date)) %>%
-  mutate(clock_start_date = if_else(Admis_Method_Desc %in% c("ELECTIVE WAITING LIST", "ELECTIVE BOOKED") & (clock_stop_date < dta_date), dta_date, clock_start_date)) %>%
-  mutate(days_to_dta = ifelse(Admis_Method_Desc == "ELECTIVE PLANNED", NA, dta_date - clock_start_date)) %>%
-  mutate(days_to_dta = ifelse(days_to_dta < 0, 0, days_to_dta)) %>%
-  mutate(days_to_dta = ifelse(is.na(days_to_dta) & Admis_Method_Desc %in% c("ELECTIVE WAITING LIST", "ELECTIVE BOOKED") , 0, days_to_dta)) %>%
-  mutate(
-    covid_recovery_priority = ifelse(
-      covid_recovery_priority == 'Planned',
-      'Planned',
-      paste0("Priority ", covid_recovery_priority)
+    parameters <- rbind(
+      parameters,
+      data.frame(
+        speciality = j,
+        time_period = labels[d],
+        metric = "demand",
+        list = demand_mean$covid_recovery_priority,
+        month_mean = demand_mean$month_mean
+      )
     )
-  ) %>%
-  mutate(covid_recovery_priority = ifelse(covid_recovery_priority == "Priority 1", "Priority 2", covid_recovery_priority)) %>%
-  mutate(covid_recovery_priority = ifelse(grepl('Pri*', covid_recovery_priority), "Inpatient_wl", as.character(covid_recovery_priority)))
+}
+
+# NEED TO GO BACK TO DRAWING BOARD FOR CAPACITY... do I go back to original QVD and remake the dataframe? This will also help with Snowflake transition
+# Capacity: this is the number of people admitted during the period
+for (j in speciality){
+capacity_daily <- data_recent %>%
+  mutate(admitted = if_else(is.na(admission_date_dt) | is.na(removed_date), 0, 1)) %>%
+  filter(admitted == 1,
+         speciality == j) %>% 
+  group_by(covid_recovery_priority, admission_date_dt) %>%
+  summarise(capacity = n()) %>%
+  ungroup() %>%
+  group_by(covid_recovery_priority) %>%
+  pad() %>%
+  mutate(capacity = if_else(is.na(capacity), 0, as.numeric(capacity))) %>%
+  filter(admission_date_dt >= (train_halt - 30))
+
+for (wl in wl_type) {
+  if (!wl %in% capacity_daily$covid_recovery_priority) {
+    demand_daily <- rbind(
+      capacity_daily,
+      data.frame(covid_recovery_priority = wl,
+                 removed_date = train_halt,
+                 capacity = 0)
+    )
+  }
+}
+capacity_mean <- capacity_daily %>%
+  group_by(covid_recovery_priority) %>%
+  summarise(month_mean = sum(capacity)/30)
+
+ggplot(data = capacity_daily, aes(x = admission_date_dt, y = capacity, colour = covid_recovery_priority)) +
+  geom_smooth(se = F)
+
+parameters <- rbind(
+  parameters,
+  data.frame(
+    speciality = j,
+    time_period = labels[d],
+    metric = "capacity",
+    list = capacity_mean$covid_recovery_priority,
+    month_mean = capacity_mean$month_mean
+  )
+)
+}
+}
